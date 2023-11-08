5fb22ad840cd5ef9e637090fd87083922c395fbf
Abhishek Aggarwal <abhishek.aggarwal836@gmail.com>
2023-11-09 01:25:09 +0530

    edit course done

--- learnify-server/controllers/course.controller.ts
+++ learnify-server/controllers/course.controller.ts
@@ -4,6 +4,7 @@ import ErrorHandler from "../utils/ErrorHandler";
 
 import cloudinary from "cloudinary";
 import { createCourse } from "../services/course.service";
+import CourseModel from "../models/course.model";
 
 // upload course
 export const uploadCourse = CatchAsyncError(
@@ -27,4 +28,43 @@ export const uploadCourse = CatchAsyncError(
             return next(new ErrorHandler(error.message, 500));
         }
     }
-)
+);
+
+// edit course
+export const editCourse = CatchAsyncError(
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const data = req.body;
+
+            const thumbnail = data.thumbnail;
+            if (thumbnail) {
+                await cloudinary.v2.uploader.destroy(thumbnail.public_id);
+
+                const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+                    folder: "courses",
+                });
+
+                data.thumbnail = {
+                    public_id: myCloud.public_id,
+                    url: myCloud.secure_url,
+                }
+            }
+
+            const courseId = req.params.id;
+
+            const course = await CourseModel.findByIdAndUpdate(
+                courseId,
+                { $set: data },
+                { new: true }
+            );
+
+            res.status(201).json({
+                success: true,
+                course,
+            });
+        }
+        catch (error: any) {
+            return next(new ErrorHandler(error.message, 500));
+        }
+    }
+);
