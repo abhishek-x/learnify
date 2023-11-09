fdba45ea4880072d2239d4819d85ec8319fec485
Abhishek Aggarwal <abhishek.aggarwal836@gmail.com>
2023-11-09 14:48:24 +0530

    get single course without purchasing done

--- learnify-server/controllers/course.controller.ts
+++ learnify-server/controllers/course.controller.ts
@@ -68,3 +68,20 @@ export const editCourse = CatchAsyncError(
         }
     }
 );
+
+// get single course -- without purchasing
+export const getSingleCourse = CatchAsyncError(
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const course = await CourseModel.findById(req.params.id).select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links");
+
+            res.status(201).json({
+                success: true,
+                course,
+            });
+        }
+        catch (error: any) {
+            return next(new ErrorHandler(error.message, 500));
+        }
+    }
+);
--- learnify-server/routes/course.route.ts
+++ learnify-server/routes/course.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { editCourse, uploadCourse } from '../controllers/course.controller';
+import { editCourse, getSingleCourse, uploadCourse } from '../controllers/course.controller';
 import { authorizeRoles, isAuthenticated } from '../middlewares/auth';
 
 const courseRouter = express.Router();
 
 courseRouter.post("/create-course", isAuthenticated, authorizeRoles("admin"), uploadCourse);
 courseRouter.put("/edit-course/:id", isAuthenticated, authorizeRoles("admin"), editCourse);
+courseRouter.get("/get-course/:id", getSingleCourse);
 
 export default courseRouter;
