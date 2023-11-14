ccee3b08c18affc3ec9503dd079a07c0d78b813a
Abhishek Aggarwal <abhishek.aggarwal836@gmail.com>
2023-11-15 00:20:50 +0530

    update notification status done

--- learnify-server/controllers/notification.controller.ts
+++ learnify-server/controllers/notification.controller.ts
@@ -9,7 +9,34 @@ export const getNotifications = CatchAsyncError(
     async (req: Request, res: Response, next: NextFunction) => {
         try {
             const notifications = await NotificationModel.find().sort({ createdAt: -1 });
-            res.status(200).json({
+            res.status(201).json({
+                success: true,
+                notifications,
+            });
+        }
+        catch (error: any) {
+            return next(new ErrorHandler(error.message, 500));
+        }
+    }
+);
+
+// update notification status -- only admin
+export const updateNotification = CatchAsyncError(
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const notification = await NotificationModel.findById(req.params.id);
+
+            if (!notification) {
+                return next(new ErrorHandler("Notification not found", 404));
+            } else {
+                notification.status ? notification.status = "read" : notification.status;
+            }
+
+            await notification.save();
+
+            const notifications = await NotificationModel.find().sort({ createdAt: -1 });
+
+            res.status(201).json({
                 success: true,
                 notifications,
             });
