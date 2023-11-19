49a8732e749b13636a8e58226b3f03b8986a15a0
Abhishek Aggarwal <abhishek.aggarwal836@gmail.com>
2023-11-19 20:34:56 +0530

    delete user done

--- learnify-server/controllers/user.controller.ts
+++ learnify-server/controllers/user.controller.ts
@@ -422,3 +422,28 @@ export const updateUserRole = CatchAsyncError(
         }
     }
 );
+
+// delete user -- only admin
+export const deleteUser = CatchAsyncError(
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const { id } = req.params;
+
+            const user = await userModel.findById(id);
+            if (!user) {
+                return next(new ErrorHandler("User not found", 404));
+            }
+
+            await user.deleteOne({ id });
+            await redis.del(id);
+
+            res.status(201).json({
+                success: true,
+                message: 'User deleted successfully',
+            });
+        }
+        catch (error: any) {
+            return next(new ErrorHandler(error.message, 400));
+        }
+    }
+);
